351594f5dc4ca400d44a21c54e12463ef4b82bde
Erblin <erblin.mazari@etu.uca.fr>
2025-03-10 11:54:49 +0100

    user list OK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace App\Controller;
-
+use App\Entity\Artiste;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,33 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer la liste des emails des artistes
+        $users = $this->getUserList($entityManager);
+
         return $this->render('user/user.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $users,
         ]);
+    }
+
+    private function getUserList(EntityManagerInterface $entityManager): array
+    {
+        // Récupérer uniquement les emails depuis la base de données
+        return $entityManager->getRepository(User::class)->createQueryBuilder('a')
+            ->select('a.email')
+            ->getQuery()
+            ->getSingleColumnResult();
+    }
+
+    #[Route('/user/{id}', name: 'app_user_by_id')]
+    public function getUserById(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $artiste = $entityManager->getRepository(Artiste::class)->find($id);
+
+        if (!$artiste) {
+            return $this->json(['error' => 'Utilisateur non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+        return $this->json($artiste);
     }
 }
